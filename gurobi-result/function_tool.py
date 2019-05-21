26068bc3b1c9b45265ab37b44a1d0c8e6bedc1b9
pandadao <cgsh112@gmail.com>
2019-05-21 21:08:38 +0800

    可從檔案讀取TT資訊並成功進行排程

--- gurobi-result/function_tool.py
+++ gurobi-result/function_tool.py
@@ -1,3 +1,4 @@
+# 找出不重複的所有解
 import copy
 
 def combine(l,n):
@@ -13,3 +14,29 @@ def combine(l,n):
             next_c(lj+1, ni+1)
     next_c()
     return answer
+
+
+#找出LCM公式
+from functools import reduce
+from math import gcd
+
+def lcm(a,b):
+    return int(a*b /gcd(a,b))
+
+def lcms(*numbers):
+    return reduce(lcm, numbers)
+
+
+def read_tt_information():
+
+    n = 0
+    for i in fp:
+        globals()["tt{}".format(n+1)] = []
+        a = "tt"+ str(n)
+        tti = eval(a)
+        period, length, number = i.split(" ")
+        tti.append(period)
+        tti.append(length)
+        tti.append(number)
+        n = n+1
+        print(eval(tti[2]))
--- gurobi-result/schedule6633.py
+++ gurobi-result/schedule6633.py
@@ -1,30 +1,56 @@
 from gurobipy import *
-from function_tool import combine
+from function_tool import combine, lcms
 
 m = Model('Protorype example_type1')
 
+
+'''
 tt1 = [6,1,1]
 tt2 = [6,1,2]
 tt3 = [3,1,3]
 tt4 = [3,1,4]
-
+'''
 '''
 6633排程
 '''
 M = 10000  # M是一個極大值,用來運作 cplex中的either-or 工能
-hyper_period = 6 # LCM求出的最大週期,先以常數項代替
+hyper_period = 1 # LCM求出的最大週期,先以常數項代替
 x_number = 0 # cplex的變數量, x1,x2,x3等
 c_number = 0
 
+
+#將tt資訊讀取出來並儲存成陣列
+n = 0
+tmp_list = []
+tt_count = []
+fp = open("tt_information.txt",'r')
+for i in fp:
+    globals()["tt{}".format(n+1)] = []
+    a = "tt"+str(n+1)
+    tti = eval(a)
+    period, length, number = i.rstrip('\n').split(" ")
+    tti.append(int(period))
+    tti.append(int(length))
+    tti.append(int(number))
+    tmp_list.append(tti[0])
+    tt_count.append(n+1)
+    n = n+1
+
+
 #取所有的tt組合配對: 1,2 1,3 1,4 2,3 2,4 3,4
-pair = combine([1,2,3,4], 2)
+#pair = combine([1,2,3,4], 2)
+pair = combine(tt_count,2)
+#a = 1
+for i in range(len(tmp_list)):
+    hyper_period = lcms(int(tmp_list[i]), hyper_period)
+    #print("hyper_period is : ",hyper_period)
 
 #宣告每個TT的offset變數
-for i in range(4):
+for i in range(n):
     a = "tt"+str(i+1)
     tti = eval(a)
     va = "x"+str(i+1)
-    globals()['x{}'.format(i+1)] = m.addVar(lb = 0,ub = int(tti[0]-1),vtype = GRB.INTEGER, name = va)
+    globals()['x{}'.format(i+1)] = m.addVar(lb = 0,ub = int(int(tti[0])-1),vtype = GRB.INTEGER, name = va)
     tti.append(eval(va))
     #print("tt list :", tti)
     x_number = x_number+1
@@ -38,8 +64,8 @@ for i in pair:
     ttj = eval(tt_j)
     print("tti: ", tti)
     print("ttj: ",ttj)
-    a = int(hyper_period/tti[0])
-    b = int(hyper_period/ttj[0])
+    a = int(hyper_period/int(tti[0]))
+    b = int(hyper_period/int(ttj[0]))
     print("a=",a)
     print("b=",b)
     for k in range(a):
@@ -52,7 +78,7 @@ for i in pair:
             #宣告c0 c1 c2編號
             ca = "c"+str(c_number)
             c_number = c_number+1
-            # //TODO:如何把x1和tt1結合起來,產生限制式
+            # 把x1和tt1結合起來,產生限制式
             m.addConstr(ttj[3]-tti[3]+l*ttj[0]-k*tti[0]-M*eval(va)<=-1, ca)
             #宣告c0 c1 c2編號
             ca = "c"+str(c_number)
@@ -62,9 +88,9 @@ for i in pair:
             #print("x_number: => ",x_number)
 
 
+m.setObjective(x1+x2+2*x3+2*x4+49.2,GRB.MAXIMIZE)
 m.update()
 m.optimize()
-m.setObjective(x1+x2+2*x3+2*x4+49.2,GRB.MAXIMIZE)
 
 print('obj: %d'% m.objVal)
 
